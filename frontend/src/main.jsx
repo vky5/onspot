ed8898eb9ca1375cdd59ca1b138040a45d283154
vky5 <vky05@proton.me>
2024-04-17 02:43:14 +0530

    routes done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,65 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import IRC from "./pages/IRC";
+import Blog from "./pages/Blog";
+import triangle from './assets/backgroundimg.png'
+import Write from "./pages/Write";
+
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Outlet
+
+} from "react-router-dom"
+ 
+const Structure = () => {
+
+  const backgroundStyle = {
+    backgroundImage: `url(${triangle})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
+
+  return (
+    <div 
+        className="" 
+        // style={backgroundStyle}
+    >
+      <Navbar />
+      <Outlet />  
+      <Footer />
+    </div>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Structure />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },{
+        path: "/blogs/:blogId",
+        element: <Blog/>
+      },{
+        path: "/write",
+        element: <Write/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
--- frontend/src/pages/Write.jsx
+++ frontend/src/pages/Write.jsx
@@ -4,24 +4,32 @@ import Textar from "../components/Textar";
 import markdownIt from "markdown-it"; //using markdownIt because it is much more safe
 
 function Write() {
+
+
   const [markdown, setMarkdown] = useState('##### Hello, Markdown!');
   // const md = new MarkdownRenderer();
-  const md = new markdownIt();
 
+  const md = new markdownIt();
   useEffect(()=>{
-    function fun(){
-      console.log(md.render(markdown))
+    function updateMD(){
+      let newDiv = document.createElement('div');
+      newDiv.innerHTML = md.render(markdown);
+
+      const ele = document.getElementById('home');
+      ele.innerHTML = "";
+      ele.appendChild(newDiv);
     }
-    fun()
+
+    updateMD();
+    
   }, [markdown])
+
   return (
     <div className="min-w-full md:flex space-x-7 min-h-screen">
       <div className="flex flex-col items-center ">
         <Textar handleChange={setMarkdown}/>
       </div>
-      <div className="" id="home ml-30"> 
-        <div dangerouslySetInnerHTML={{ __html: md.render(markdown) }} />
-      </div>
+      <div className="" id="home"/> 
     </div>
   );
 }
