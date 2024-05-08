1262321892f3c27024d367519009f8b9663c43d5
vky5 <vky05@proton.me>
2024-05-08 15:21:21 +0530

    conditionally render footer for signin and signup

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import logo from "../assets/logo.png";
 import dark_mode from "../assets/dark_mode_button.png";
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
 import {
   createBrowserRouter, 
   RouterProvider, 
-  Outlet
-} from 'react-router-dom'
+  Outlet,
+  useLocation
+} from 'react-router-dom';
 
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Blogs from './pages/Blogs'
-import BlogTemp from './pages/BlogTemp'
-import Liked from './pages/Liked'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Blogs from './pages/Blogs';
+import BlogTemp from './pages/BlogTemp';
+import Liked from './pages/Liked';
 import About from './pages/About';
-import Profile from './pages/Profile'
-import Footer from './components/Footer'
+import Profile from './pages/Profile';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 
@@ -25,53 +26,68 @@ const paths = [
     element: (
       <Home />
     )
-  },{
+  },
+  {
     path: '/blogs',
     element: (
       <Blogs />
     )
-  },{
+  },
+  {
     path: '/blogs/:id',
     element: (
       <BlogTemp />
     )
-  },{
+  },
+  {
     path: '/liked',
     element: (
       <Liked />
     )
-  },{
+  },
+  {
     path: '/about',
     element: (
       <About />
     )
-  },{
+  },
+  {
     path: '/profile',
     element: (
       <Profile />
     )
-  },{
+  },
+  {
     path: '/login',
     element: (
       <Signin />
     )
-  },{
+  },
+  {
     path: '/signup',
     element: (
       <Signup />
     )
   }
-]
+];
 
-const AppComponent = ()=>{
+const AppComponent = () => {
+  const location = useLocation();
+  
+  // Define routes where Navbar and Footer should not appear
+  const excludedRoutes = ['/login', '/signup'];
+  
+  // Check if the current route is excluded
+  const isExcludedRoute = excludedRoutes.includes(location.pathname);
+  
   return (
     <div>
-      <Navbar />
-      <Outlet/>
-      <Footer />
+      {!isExcludedRoute && <Navbar />}
+      <Outlet />
+      {!isExcludedRoute && <Footer />}
     </div>
-  )
-}
+  );
+};
 
 const appRouter = createBrowserRouter([
   {
@@ -85,4 +101,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={appRouter}/>
   </React.StrictMode>,
-)
+);
