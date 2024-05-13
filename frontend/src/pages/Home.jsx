5b2190cff5939c718b6f08f82348bf3d660c6433
vky5 <vky05@proton.me>
2024-05-13 07:46:00 +0530

    fixed blogcard space

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import MyCarousel from "../components/HomeComponents/MyCarousel";
 import BlogCard from "../components/BlogCard";
 
-
 function Home() {
   return (
     <div className="bg-gray-100 pb-6">
@@ -12,11 +11,16 @@ function Home() {
           <div className="font-bold text-xl">Branching Perspectives</div>
         </div>
       </div>
-      <div className="pt-12"> {/*here is the css for carousel height in different screen size*/}
+      <div className="pt-12">
+        {" "}
+        {/*here is the css for carousel height in different screen size*/}
         <MyCarousel />
       </div>
       <div className="pt-6">
-        <ul className="flex px-4 list-none space-x-7 scrollbar overflow-auto text-xs font-medium" style={{ scrollbarWidth: 'none' }}>
+        <ul
+          className="flex px-4 list-none space-x-7 scrollbar overflow-auto text-xs font-medium"
+          style={{ scrollbarWidth: "none" }}
+        >
           <li>Latest</li>
           <li>Trending</li>
           <li>Blockhain</li>
@@ -26,8 +30,16 @@ function Home() {
           <li>System Design</li>
         </ul>
       </div>
-      <div className="bg-white rounded-lg overflow-hidden shadow-md pl-3 pr-3 ml-2 mr-2">
-        <BlogCard heading="This is the introductory blog post and are going to build something unique what we are trying to build is so unique that it is unique"/>
+      <div className="space-y-3 mt-3">
+        <div className="bg-white rounded-lg overflow-hidden shadow-md pl-3 pr-3 ml-2 mr-2">
+          <BlogCard heading="This is the introductory blog post and are going to build something unique what we are trying to build is so unique that it is unique" />
+        </div>
+        <div className="bg-white rounded-lg overflow-hidden shadow-md pl-3 pr-3 ml-2 mr-2">
+          <BlogCard heading="This is the introductory blog post and are going to build something unique what we are trying to build is so unique that it is unique" />
+        </div>
+        <div className="bg-white rounded-lg overflow-hidden shadow-md pl-3 pr-3 ml-2 mr-2">
+          <BlogCard heading="This is the introductory blog post and are going to build something unique what we are trying to build is so unique that it is unique" />
+        </div>
       </div>
     </div>
   );
