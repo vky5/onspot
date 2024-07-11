ec298f790be5bb5c3504fc8e27b2cfade6f48827
vky5 <vky05@proton.me>
2024-07-11 21:25:30 +0530

    refactored commentForAPost controllers

--- backend/controllers/commentControllers.js
+++ backend/controllers/commentControllers.js
@@ -1,115 +1,106 @@
-const CommentModel = require('../model/commentModel');
-const AppError = require('../utils/appError');
-const catchAsync = require('../utils/catchAsync');
+const CommentModel = require("../model/commentModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
 
-const postComment = catchAsync(async (req, res, next)=>{
-    let blogid = req.params.blogid;
-    if (!req.params.blogid) blogid = req.body.blogid;
-    const addedComment = await CommentModel.create({
-        text: req.body.text,
-        user: req.user._id, // get the user from request object
-        post: blogid
-    });
+const postComment = catchAsync(async (req, res, next) => {
+  let blogid = req.params.blogid;
+  if (!req.params.blogid) blogid = req.body.blogid;
+  const addedComment = await CommentModel.create({
+    text: req.body.text,
+    user: req.user._id, // get the user from request object
+    post: blogid,
+  });
 
-    if (!addedComment){ return next(new AppError('Something Unexpected happened', 500))};
+  if (!addedComment) {
+    return next(new AppError("Something Unexpected happened", 500));
+  }
 
-    res.status(201).json({
-        status: 'success',
-        comment: addedComment
-    })
-})
+  res.status(201).json({
+    status: "success",
+    comment: addedComment,
+  });
+});
 
 const updateComment = catchAsync(async (req, res, next) => {
-    const updateComment = await CommentModel.findByIdAndUpdate(
-        req.params.commentid,
-        { text: req.body.text }, 
-        { runValidators: true, new: true }
+  const updateComment = await CommentModel.findByIdAndUpdate(
+    req.params.commentid,
+    { text: req.body.text },
+    { runValidators: true, new: true }
+  );
+
+  if (!updateComment) return next(new AppError("No comment found", 400));
+  if (updateComment.user !== req.user._id && req.user.role !== "admin")
+    return next(
+      new AppError("You are not authorized to make these changes", 403)
     );
 
-    if (!updateComment) return next(new AppError('No comment found', 400));
-    if (updateComment.user!==req.user._id && req.user.role!=='admin') return next(new AppError('You are not authorized to make these changes', 403))
-
-    res.status(200).json({
-        status: 'success',
-        comment: updateComment
-    });
+  res.status(200).json({
+    status: "success",
+    comment: updateComment,
+  });
 });
 
+const deleteComment = catchAsync(async (req, res, next) => {
+  const deleteComment = await CommentModel.findByIdAndDelete(
+    req.params.commentid
+  );
 
-const deleteComment = catchAsync(async (req, res, next)=>{
-    const deleteComment = await CommentModel.findByIdAndDelete(req.params.commentid);
+  if (!deleteComment) {
+    return next(new AppError("No comment found", 400));
+  }
+  if (updateComment.user !== req.user._id && req.user.role !== "admin")
+    return next(
+      new AppError("You are not authorized to make these changes", 403)
+    );
 
-    if (!deleteComment){
-        return next(new AppError('No comment found', 400));
-    }
-    if (updateComment.user!==req.user._id && req.user.role!=='admin') return next(new AppError('You are not authorized to make these changes', 403))
+  res.status(200).json({
+    status: "success",
+    deletedComment: deleteComment,
+  });
+});
 
-    res.status(200).json({
-        status: 'success',
-        deletedComment: deleteComment
-    })
-})
+const getCommentsForUser = catchAsync(async (req, res, next) => {
+  const allComments = await CommentModel.find({
+    user: req.user._id,
+  });
 
-const getCommentsForUser = catchAsync(async (req, res, next)=>{
-    const allComments = await CommentModel.find({
-        user: req.user._id
-    });
-
-    res.status(200).json({
-        status: 'success',
-        comments: allComments
-    })
-})
+  res.status(200).json({
+    status: "success",
+    comments: allComments,
+  });
+});
 
 const getCommentForPost = catchAsync(async (req, res, next) => {
-    let blogid = req.params.blogid;
-    if (!blogid) blogid = req.body.blogid;
-    
-    const allComments = await CommentModel.aggregate([
-        {
-            $match: { post: blogid } 
-        },
-        {
-            $lookup: {
-                from: 'userdatas', 
-                localField: 'username',
-                foreignField: 'username',
-                as: 'user' 
-            }
-        },
-        {
-            $unwind: '$user'
-        },
-        {
-            $project: { 
-                _id: 0, 
-                commentId: 1,
-                text: 1,
-                postId: 1,
-                'user.username': 1,
-                'user.name': 1 
-            }
-        }
-    ]);
+  let blogid = req.params.blogid;
+  if (!blogid) blogid = req.body.blogid;
 
-    res.status(200).json({
-        status: 'success',
-        comments: allComments
-    });
+  const allComments = await CommentModel.find({
+    blog: blogid,
+  });
+
+  res.status(200).json({
+    status: "success",
+    comments: allComments,
+  });
 });
 
+const getParticularComment = catchAsync(async (req, res, next) => {
+  const getComment = await CommentModel.findById(req.params.commentid);
 
+  if (!getComment) return next(new AppError("No Comment found", 400));
+  res.status(200).json({
+    status: "success",
+    comment: getComment,
+  });
+});
 
-const getParticularComment = catchAsync(async (req, res, next)=>{
-    const getComment = await CommentModel.findById(req.params.commentid);
-
-    if (!getComment) return next(new AppError('No Comment found', 400));
-    res.status(200).json({
-        status: 'success',
-        comment: getComment
-    })
-})
-
-module.exports = {postComment, updateComment, deleteComment, getCommentsForUser, getCommentForPost, getParticularComment};
+module.exports = {
+  postComment,
+  updateComment,
+  deleteComment,
+  getCommentsForUser,
+  getCommentForPost,
+  getParticularComment,
+};
 
 // the beautiful thing about this is you will only have access to mongoDB ids of comments for your own comments
